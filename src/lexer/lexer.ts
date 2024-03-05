c3d8d66afcd6700ea01a1ffb12c7bf6686a1a31f
leepavelich <leepavelich@gmail.com>
2024-03-05 09:56:38 -0700

    p. 20

--- src/lexer/lexer.ts
+++ src/lexer/lexer.ts
@@ -1,5 +1,34 @@
 import { Token, TokenItem, TokenType } from "../token/token";
 
+function createToken(type: TokenItem, literal: string): Token {
+  return { type, literal };
+}
+
+const _0 = "0".charCodeAt(0);
+const _9 = "9".charCodeAt(0);
+
+const a = "a".charCodeAt(0);
+const z = "z".charCodeAt(0);
+const A = "A".charCodeAt(0);
+const Z = "Z".charCodeAt(0);
+
+const _ = "_".charCodeAt(0);
+
+function isLetter(char: string): boolean {
+  const ch = char.charCodeAt(0);
+  return (a <= ch && ch <= z) || (A <= ch && ch <= Z) || ch === _;
+}
+
+function isDigit(char: string): boolean {
+  const ch = char.charCodeAt(0);
+  return _0 <= ch && ch <= _9;
+}
+
+const Keywords = {
+  fn: createToken(TokenType.Function, "fn"),
+  let: createToken(TokenType.Let, "let"),
+} as const;
+
 export class Lexer {
   private position: number = 0;
   private readPosition: number = 0;
@@ -10,35 +39,53 @@ export class Lexer {
   }
 
   public getNextToken(): Token {
-    let tok: Token;
+    this.skipWhitepsace();
+
+    let tok: Token | undefined;
 
     switch (this.ch) {
       case "=":
-        tok = this.createToken(TokenType.Assign, this.ch);
+        tok = createToken(TokenType.Assign, this.ch);
         break;
       case ";":
-        tok = this.createToken(TokenType.Semicolon, this.ch);
+        tok = createToken(TokenType.Semicolon, this.ch);
         break;
       case "(":
-        tok = this.createToken(TokenType.LParen, this.ch);
+        tok = createToken(TokenType.LParen, this.ch);
         break;
       case ")":
-        tok = this.createToken(TokenType.RParen, this.ch);
+        tok = createToken(TokenType.RParen, this.ch);
         break;
       case ",":
-        tok = this.createToken(TokenType.Comma, this.ch);
+        tok = createToken(TokenType.Comma, this.ch);
         break;
       case "+":
-        tok = this.createToken(TokenType.Plus, this.ch);
+        tok = createToken(TokenType.Plus, this.ch);
         break;
       case "{":
-        tok = this.createToken(TokenType.LBrace, this.ch);
+        tok = createToken(TokenType.LBrace, this.ch);
         break;
       case "}":
-        tok = this.createToken(TokenType.RBrace, this.ch);
+        tok = createToken(TokenType.RBrace, this.ch);
         break;
-      default:
-        tok = this.createToken(TokenType.Eof, "");
+      case "\0":
+        tok = createToken(TokenType.Eof, "");
+        break;
+    }
+
+    if (isLetter(this.ch)) {
+      const ident = this.readIdentifier();
+      const keyword = Keywords[ident as keyof typeof Keywords];
+      if (keyword) {
+        return keyword;
+      } else {
+        return createToken(TokenType.Ident, ident);
+      }
+    } else if (isDigit(this.ch)) {
+      const digit = this.readNumber();
+      return createToken(TokenType.Int, digit);
+    } else if (!tok) {
+      return createToken(TokenType.Illegal, this.ch);
     }
 
     this.readChar();
@@ -55,7 +102,34 @@ export class Lexer {
     this.readPosition += 1;
   }
 
-  private createToken(type: TokenItem, literal: string): Token {
-    return { type, literal };
+  private readIdentifier(): string {
+    const position = this.position;
+
+    while (isLetter(this.ch)) {
+      this.readChar();
+    }
+
+    return this.input.slice(position, this.position);
+  }
+
+  private readNumber(): string {
+    const position = this.position;
+
+    while (isDigit(this.ch)) {
+      this.readChar();
+    }
+
+    return this.input.slice(position, this.position);
+  }
+
+  private skipWhitepsace(): void {
+    while (
+      this.ch == " " ||
+      this.ch == "\t" ||
+      this.ch == "\n" ||
+      this.ch == "\r"
+    ) {
+      this.readChar();
+    }
   }
 }
